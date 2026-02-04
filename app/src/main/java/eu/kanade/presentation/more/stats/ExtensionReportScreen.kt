0efe4799ec5d079935f5be32d1451d23ca8dc618
salmanbappi <salmanbappi@users.noreply.github.com>
2026-02-04 14:48:26 +0600

    fix(infra): import alpha in ExtensionReportScreen

--- app/src/main/java/eu/kanade/presentation/more/stats/ExtensionReportScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/stats/ExtensionReportScreen.kt
@@ -29,6 +29,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
