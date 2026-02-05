ed614de85da261cca3abf10e056e537e7c596d7f
salmanbappi <salmanbappi@users.noreply.github.com>
2026-02-06 04:47:10 +0600

    fix: revert global dynamic theme and keep it local to details screen

--- app/src/main/java/eu/kanade/tachiyomi/util/view/ViewExtensions.kt
+++ app/src/main/java/eu/kanade/tachiyomi/util/view/ViewExtensions.kt
@@ -20,22 +20,15 @@ import androidx.compose.runtime.CompositionContext
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.ViewCompositionStrategy
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import eu.kanade.presentation.theme.DynamicTachiyomiTheme
 import eu.kanade.presentation.theme.TachiyomiTheme
 import eu.kanade.tachiyomi.R
-import eu.kanade.tachiyomi.util.system.CoverColorObserver
 
 inline fun ComponentActivity.setComposeContent(
     parent: CompositionContext? = null,
     crossinline content: @Composable () -> Unit,
 ) {
     setContent(parent) {
-        val vibrantColors by CoverColorObserver.vibrantColors.collectAsState()
-        val latestColor = vibrantColors.values.lastOrNull()
-
-        DynamicTachiyomiTheme(colorSeed = latestColor) {
+        TachiyomiTheme {
             CompositionLocalProvider(
                 LocalTextStyle provides MaterialTheme.typography.bodySmall,
                 LocalContentColor provides MaterialTheme.colorScheme.onBackground,
@@ -51,10 +44,7 @@ fun ComposeView.setComposeContent(
 ) {
     setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
     setContent {
-        val vibrantColors by CoverColorObserver.vibrantColors.collectAsState()
-        val latestColor = vibrantColors.values.lastOrNull()
-
-        DynamicTachiyomiTheme(colorSeed = latestColor) {
+        TachiyomiTheme {
             CompositionLocalProvider(
                 LocalTextStyle provides MaterialTheme.typography.bodySmall,
                 LocalContentColor provides MaterialTheme.colorScheme.onBackground,
