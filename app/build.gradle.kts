366c48a87f0b56d8bb9fcde5ce7f50b3cca6e6bf
salmanbappi <salmanbappi@users.noreply.github.com>
2026-02-07 18:41:12 +0000

    chore: bump version to 0.1.236-MOD

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,8 +53,8 @@ if (Config.includeTelemetry && file("google-services.json").exists()) {
 
         applicationId = "app.anizen"
 
-        versionCode = 235
-        versionName = "0.1.235-MOD"
+        versionCode = 236
+        versionName = "0.1.236-MOD"
 
         manifestPlaceholders["author"] = "@salmanbappi"
 
